84c6a218a38eebf6a91ccc0164b2ad85ea23cb36
Teddy Seddon <teddy.seddon@icloud.com>
2023-05-02 23:48:34 +0100

    Further SettingsScreenTests

--- PiazzaPanic/tests/src/com/mygdx/tests/ScreenTests/SettingsScreenTests.java
+++ PiazzaPanic/tests/src/com/mygdx/tests/ScreenTests/SettingsScreenTests.java
@@ -8,12 +8,10 @@ import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.PiazzaPanic;
-import com.mygdx.game.Screens.GameScreen;
 import com.mygdx.game.Screens.SettingsScreen;
 import com.mygdx.tests.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
